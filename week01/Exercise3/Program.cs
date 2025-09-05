478e959db89dcc380e8a1aba1726b9daa1e3526a
GutsySpider <bowman.p.michael@gmail.com>
2025-09-04 19:02:33 -0700

    Created guessing game with loops.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is the Magic Number? ");
+        // string userMagic = Console.ReadLine();
+        // int magicNumber = int.Parse(userMagic);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int magicGuess = -1;
+
+        while (magicGuess != magicNumber)
+        {
+            Console.Write("What is your guess? (1-100) ");
+            string userGuess = Console.ReadLine();
+            magicGuess = int.Parse(userGuess);
+
+            if (magicNumber > magicGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < magicGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }

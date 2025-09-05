aee318faaae607b5b3c7a9e54d38dc99fff7efb5
GutsySpider <bowman.p.michael@gmail.com>
2025-09-04 21:25:35 -0700

    Adjusted code to make a list and do calculations.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
+
+
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int enteredNumber = -1;
+
+        while (enteredNumber != 0)
+        {
+            Console.Write("Enter Number (enter 0 to quit):");
+            string userInput = Console.ReadLine();
+            enteredNumber = int.Parse(userInput);
+
+            if (enteredNumber != 0)
+            {
+                numbers.Add(enteredNumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"Your sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"Your average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"Your max is: {max}");
     }
 }

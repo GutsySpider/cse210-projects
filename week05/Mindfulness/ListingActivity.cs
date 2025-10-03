28eee51f72fb53e3d1694fabaebf12387062154e
GutsySpider <bowman.p.michael@gmail.com>
2025-10-02 23:15:10 -0700

    Mindfulness Project

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,23 @@
+public class ListingActivity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity(int count, List<string> prompts)
+    {
+        _count = count;
+        _prompts = prompts;
+    }
+    public void Run()
+    {
+
+    }
+    public void GetRandomPrompt()
+    {
+
+    }
+    public List<string> GetListFromUser()
+    {
+        return new List<string> (_prompts);
+    }
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Activity a1 = new Activity("Michael", "Yelling", 5);
     }
 }

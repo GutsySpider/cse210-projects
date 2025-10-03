fa1af364167f409d03f580b610ca4424b8a910c4
GutsySpider <bowman.p.michael@gmail.com>
2025-10-02 22:39:52 -0700

    Homework Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Tony Tiger", "Spanish");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment ma1 = new MathAssignment("Michael Bowman", "Math", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(ma1.GetSummary());
+        Console.WriteLine(ma1.GetHomeworkList());
+        
+        WritingAssignment w1 = new WritingAssignment("George Lucas", "WWII", "Militia Weapons");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
